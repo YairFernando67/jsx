32b6fa49b490bd218498f6f758303f59c342b418
Yair Fernando Facio Fraga <yair.facio11@gmail.com>
2019-11-30 17:47:00 -0600

    Adding some jsx to the component

--- src/index.js
+++ src/index.js
@@ -4,8 +4,25 @@ import ReactDOM from 'react-dom';
 // const React = require('react');
 
 // Create a REACT Component
+function getCurrentTime() {
+  return (new Date()).toLocaleTimeString();
+}
 const App = () => {
-  return <div>Hi there!</div>
+  const buttonText = { text: 'click me'};
+  const labelText = 'Enter name: '
+  return (
+    <div>
+      <label htmlFor="name" className="label">{labelText}</label>
+      <input id="name" type="text"/>
+      <button style={{ backgroundColor: "#ccc", color: "white", border: "2px solid #444"}}>
+        {buttonText.text}
+      </button>
+      <div>
+        <h1>Current Time</h1>
+        <p>{getCurrentTime()}</p>
+      </div>
+    </div>
+  );
 };
 
 // Take the REACT Component and show it on the screen
